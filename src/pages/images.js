2c77ff3b68fd7dc0816751c14c390f64b5cfda96
lmalunin@gmail.com <lmalunin@gmail.com>
2019-08-07 04:35:47 +0300

    44

--- src/pages/images.js
+++ src/pages/images.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Layout from './../components/layout'
+import Images from './../examples/Images'
 
 const images = () => {
     return (
         <Layout>
-
+            <Images />
         </Layout>
     )
 }
